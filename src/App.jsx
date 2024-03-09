c138676c3c2260d452eb092139f31c85005de18f
Keshav Kumar <keshavkumar8122@gmail.com>
2024-03-09 17:35:52 +0530

    Kanban Board

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppProvider } from "./context/AppContext";
+import Home from "./pages/Home"
 
 function App() {
-  
+
   return (
     <>
-      
+      <AppProvider>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </AppProvider>
     </>
   )
 }
